a854ead2108b9d33f01307309af1bda29c47b7bd
KNURanasinghe <113103000+KNURanasinghe@users.noreply.github.com>
2024-02-02 02:40:20 +0530

    signin auth done

--- garbage_collecting_system/lib/main.dart
+++ garbage_collecting_system/lib/main.dart
@@ -1,7 +1,6 @@
 import 'package:awesome_notifications/awesome_notifications.dart';
 import 'package:flutter/material.dart';
-
-import 'screens/homepage.dart';
+import 'package:garbage_collecting_system/screens/signUp.dart';
 
 void main() async {
   await AwesomeNotifications().initialize(null, [
@@ -38,7 +37,7 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: const HomeScreen(),
+      home: const SignUpPage(),
     );
   }
 }
---
+++ garbage_collecting_system/lib/screens/signUp.dart
@@ -0,0 +1,118 @@
+import 'dart:convert';
+
+import 'package:flutter/material.dart';
+import 'package:garbage_collecting_system/screens/login.dart';
+import 'package:http/http.dart' as http;
+
+class SignUpPage extends StatefulWidget {
+  const SignUpPage({super.key});
+
+  @override
+  State<SignUpPage> createState() => _SignInPageState();
+}
+
+class _SignInPageState extends State<SignUpPage> {
+  TextEditingController nameController = TextEditingController();
+
+  TextEditingController passwordController = TextEditingController();
+  bool _isNotValidate = false;
+
+  void registerUser() async {
+    if (nameController.text.isNotEmpty && passwordController.text.isNotEmpty) {
+      var regBody = {
+        "email": nameController.text,
+        "password": passwordController.text
+      };
+
+      var response = await http.post(
+          Uri.parse("http://192.168.8.111:3000/registration"),
+          headers: {"Content-type": "application/json"},
+          body: jsonEncode(regBody));
+      print("somethind went wrong ....$response");
+      var jsonResponse = jsonDecode(response.body);
+      print("somethind went wrong ....$jsonResponse");
+      if (jsonResponse['status'] == true) {
+        Navigator.push(
+            context,
+            MaterialPageRoute(
+              builder: (context) => const LoginPage(),
+            ));
+      } else {
+        print("somethind went wrong");
+      }
+    } else {
+      setState(() {
+        _isNotValidate = true;
+      });
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    final size = MediaQuery.of(context).size;
+    return Scaffold(
+      body: SingleChildScrollView(
+        child: Column(
+          children: [
+            Container(
+              height: 350,
+              width: size.width,
+              decoration: const BoxDecoration(
+                  borderRadius: BorderRadius.only(
+                    bottomLeft: Radius.circular(70),
+                  ),
+                  color: Colors.blue),
+              child: const Center(
+                child: Text(
+                  "SignUp ",
+                  style: TextStyle(
+                      fontWeight: FontWeight.bold,
+                      fontSize: 40,
+                      color: Colors.white),
+                ),
+              ),
+            ),
+            const SizedBox(
+              height: 30,
+            ),
+            Padding(
+              padding: const EdgeInsets.all(16.0),
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  TextField(
+                    controller: nameController,
+                    decoration: InputDecoration(
+                        labelText: 'Name',
+                        errorText: _isNotValidate ? "fill the field" : null),
+                  ),
+                  const SizedBox(height: 16.0),
+                  TextField(
+                    controller: passwordController,
+                    obscureText: true,
+                    decoration: InputDecoration(
+                        labelText: 'Password',
+                        errorText: _isNotValidate ? "fill the field" : null),
+                  ),
+                  const SizedBox(height: 24.0),
+                  ElevatedButton(
+                    onPressed: () {
+                      // Handle sign-in logic here
+                      // String mobile = mobileController.text;
+                      // String password = passwordController.text;
+                      registerUser();
+
+                      // Perform validation and sign-in logic
+                      // print('Mobile: $mobile\nPassword: $password');
+                    },
+                    child: const Text('Sign Up'),
+                  ),
+                ],
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
