b62ef74e1fc3b853759d966a946ffcfeea9fd203
KNURanasinghe <113103000+KNURanasinghe@users.noreply.github.com>
2024-02-02 16:25:08 +0530

    done

--- garbage_collecting_system/lib/main.dart
+++ garbage_collecting_system/lib/main.dart
@@ -2,6 +2,7 @@ import 'package:awesome_notifications/awesome_notifications.dart';
 import 'package:flutter/material.dart';
 
 import 'screens/homepage.dart';
+import 'screens/signUp.dart';
 
 void main() async {
    WidgetsFlutterBinding.ensureInitialized();
@@ -35,7 +36,7 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: const HomeScreen(),
+      home: const SignUpPage(),
     );
   }
 }
